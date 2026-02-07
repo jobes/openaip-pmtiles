273152376d3f6cf9676b3e50fbae5d73bc39681d
Viktor Joba <jobes666@gmail.com>
2026-02-07 14:31:00 +0100

    au remove slow fir

--- countries.py
+++ countries.py
@@ -14,7 +14,7 @@ all_countries = ['ad','ae','af','ag','ai','al','am','ao','aq','ar','as','at','au
              'tj','tl','tm','tn','to','tr','tt','tv','tw','tz','ua','ug','us','uy','uz','vc',
              've','vg','vi','vn','vu','wf','ws','xk','ye','yt','za','zm','zw']
 
-slow_countries = ['au','gl']
+slow_countries = ['gl']
 
 quick_countries = [c for c in all_countries if c not in slow_countries]
 country_groups = {'quick_countries': quick_countries,**{c: [c] for c in slow_countries}}
@@ -22,4 +22,11 @@ country_groups = {'quick_countries': quick_countries,**{c: [c] for c in slow_cou
 slow_features = {'ar': {
     'airspaces': {'name':'FIR COMODORO'},
     'airspaces_border_offset': {'name':'FIR COMODORO'},
-    'airspaces_border_offset_2x': {'name':'FIR COMODORO'}}}
+    'airspaces_border_offset_2x': {'name':'FIR COMODORO'}
+    },'au': {
+    'airspaces': {'name':'MELBOURNE FIR CTA A2'},
+    'airspaces_border_offset': {'name':'MELBOURNE FIR CTA A2'},
+    'airspaces_border_offset_2x': {'name':'MELBOURNE FIR CTA A2'}
+    }
+    }
+
