20cb4b527883129ab8c723276eeb2e2ed41184f0
Viktor Jóba <jobes666@gmail.com>
2026-02-07 10:11:50 +0100

    Fix country_groups to use slow_countries

--- countries.py
+++ countries.py
@@ -17,4 +17,4 @@ all_countries = ['ad','ae','af','ag','ai','al','am','ao','aq','ar','as','at','au
 slow_countries = ['ar','au','gl']
 
 quick_countries = [c for c in all_countries if c not in slow_countries]
-country_groups = {'quick_countries': quick_countries,**{c: [c] for c in all_countries}}
+country_groups = {'quick_countries': quick_countries,**{c: [c] for c in slow_countries}}
