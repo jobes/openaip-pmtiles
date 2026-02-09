3a7b31da6b9ee2bb352da66882257473ad3406c3
Viktor Joba <jobes666@gmail.com>
2026-02-09 19:26:02 +0100

    add Hugging Face upload step and update dependencies in workflow

---
+++ upload_to_hugging_face.py
@@ -0,0 +1,4 @@
+import os
+from huggingface_hub import upload_file
+hf_token = os.environ.get("HF_TOKEN")
+upload_file(path_or_fileobj="openaip.pmtiles", repo_id="jobes666/openaip-mptiles", repo_type="dataset", token=hf_token)
