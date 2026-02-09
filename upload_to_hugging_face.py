ae12d6760473b80cb5d4492bee57037068c8c6cc
Viktor Joba <jobes666@gmail.com>
2026-02-09 19:28:32 +0100

    format upload_file call for clarity and consistency

--- upload_to_hugging_face.py
+++ upload_to_hugging_face.py
@@ -1,4 +1,8 @@
 import os
 from huggingface_hub import upload_file
 hf_token = os.environ.get("HF_TOKEN")
-upload_file(path_or_fileobj="openaip.pmtiles", repo_id="jobes666/openaip-mptiles", repo_type="dataset", token=hf_token)
+upload_file(path_or_fileobj="openaip.pmtiles", 
+            path_in_repo="openaip.pmtiles",
+            repo_id="jobes666/openaip-mptiles", 
+            repo_type="dataset", 
+            token=hf_token)
