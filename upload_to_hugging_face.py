dd9d5fca6f8003371d4ac2fbeac92a677aecda7f
Viktor Joba <jobes666@gmail.com>
2026-02-09 19:40:51 +0100

    add token presence check and logging in upload_to_hugging_face.py

--- upload_to_hugging_face.py
+++ upload_to_hugging_face.py
@@ -1,6 +1,12 @@
 import os
 from huggingface_hub import upload_file
 hf_token = os.environ.get("HF_TOKEN")
+
+if hf_token:
+    print(f"Token found! Token length: {len(hf_token)}")
+else:
+    print("No token found")
+
 upload_file(path_or_fileobj="openaip.pmtiles", 
             path_in_repo="openaip.pmtiles",
             repo_id="jobes666/openaip-mptiles", 
